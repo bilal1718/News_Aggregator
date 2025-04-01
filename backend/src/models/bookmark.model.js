6f69fb25d3de0a754dd201c42b303c34cab9d3e7
Muhammad Bilal <muhbil171845@gmail.com>
2025-04-01 13:59:56 +0500

    Bookmark model

---
+++ backend/src/models/bookmark.model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Bookmark = sequelize.define('Bookmark', {
+    bookmarkId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    userId: {  // Explicit foreign key
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'userId'
+      }
+    },
+    articleId: {  // Explicit foreign key
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Articles',
+        key: 'articleId'
+      }
+    }
+  }, {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  });
+
+module.exports = Bookmark;
