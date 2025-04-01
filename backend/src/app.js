eabbfbfd3f348cd4fb2ebce6174dda2aa10b9e8e
Muhammad Bilal <muhbil171845@gmail.com>
2025-04-01 13:35:39 +0500

    Adding articles

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
+const articleRoutes = require('./routes/article.routes');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/articles', articleRoutes);
 
 sequelize.authenticate()
   .then(() => {
