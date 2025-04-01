6057b83f17bfb5663879cad2f6d5d05e8d39db59
Muhammad Bilal <muhbil171845@gmail.com>
2025-04-01 13:59:27 +0500

    Define relationship

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,11 +1,20 @@
 const User = require('./user.model');
 const Article = require('./article.model');
+const Bookmark = require('./bookmark.model');
 
 // User-Article Relationship (Author)
 User.hasMany(Article, { foreignKey: 'authorId' });
 Article.belongsTo(User, { foreignKey: 'authorId' });
 
+
+Bookmark.belongsTo(User, { foreignKey: 'userId' });
+Bookmark.belongsTo(Article, { foreignKey: 'articleId' });
+
+User.hasMany(Bookmark, { foreignKey: 'userId' });
+Article.hasMany(Bookmark, { foreignKey: 'articleId' });
+
 module.exports = {
-  User,
-  Article
-};
+    User,
+    Article,
+    Bookmark
+  };
