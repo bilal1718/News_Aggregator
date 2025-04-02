bfcf17d22f7dbf04c918b2aa6231986a7bae49fe
Muhammad Bilal <muhbil171845@gmail.com>
2025-04-02 12:48:18 +0500

    Define relation between user, article and comment

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,20 +1,39 @@
 const User = require('./user.model');
 const Article = require('./article.model');
 const Bookmark = require('./bookmark.model');
+const Comment = require('./comment.model');
 
 // User-Article Relationship (Author)
 User.hasMany(Article, { foreignKey: 'authorId' });
 Article.belongsTo(User, { foreignKey: 'authorId' });
 
-
+// Bookmark relationships
 Bookmark.belongsTo(User, { foreignKey: 'userId' });
 Bookmark.belongsTo(Article, { foreignKey: 'articleId' });
-
 User.hasMany(Bookmark, { foreignKey: 'userId' });
 Article.hasMany(Bookmark, { foreignKey: 'articleId' });
 
+// Comment relationships
+Comment.belongsTo(User, { 
+  foreignKey: 'userId',
+  as: 'commenter'
+});
+Comment.belongsTo(Article, {
+  foreignKey: 'articleId',
+  as: 'article'
+});
+User.hasMany(Comment, { 
+  foreignKey: 'userId',
+  as: 'comments'
+});
+Article.hasMany(Comment, {
+  foreignKey: 'articleId',
+  as: 'comments'
+});
+
 module.exports = {
-    User,
-    Article,
-    Bookmark
-  };
+  User,
+  Article,
+  Bookmark,
+  Comment 
+};
