ee36e58c3afed4f06cf12460635e373b02eeba22
ZainJ5 <154011623+ZainJ5@users.noreply.github.com>
2025-04-14 19:21:26 +0500

    Created Article Card component

---
+++ frontend/src/components/ArticleCard.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { addBookmark, removeBookmark, addReaction } from '../services/api';
+
+const ArticleCard = ({ article, isBookmarked = false, onBookmarkChange }) => {
+;
+
+
+};
+
+export default ArticleCard;
