68a74ae37501313b6ddffd1c3ef835f85d850d33
Hussnain Abbas <hussnainabbas14725@gmail.com>
2025-05-13 23:20:09 +0500

    Update article.routes.js

--- backend/src/routes/article.routes.js
+++ backend/src/routes/article.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { fetchNews, getArticles, getArticlesByCategory } = require('../controllers/article.controller');
+const { fetchNews, getArticles, getArticlesByCategory, getArticleById } = require('../controllers/article.controller');
 
 router.get('/fetch', fetchNews);
 router.get('/', getArticles);
 router.get('/:category', getArticlesByCategory); 
+router.get('/id/:id', getArticleById);
+
 
 module.exports = router;
